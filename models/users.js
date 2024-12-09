707d0da6aa9347907f20a7e1e823b755aecb3042
HarshWebCraft <harshwebdev26@gmail.com>
2024-12-09 23:50:20 +0530

    signup name added

--- models/users.js
+++ models/users.js
@@ -1,19 +1,21 @@
-const mongoose = require('mongoose') 
-
-
+const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    Email: {
-      type: String,
-      required: true,
-      unique: true
-    },
-    Password: {
-      type: String,
-      required: true
-    }
-  });
+  Email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  Password: {
+    type: String,
+    required: true,
+  },
+  Name: {
+    type: String,
+    required: true,
+  },
+});
 
-const USER = new mongoose.model('UserCredentials',userSchema)
+const USER = new mongoose.model("UserCredentials", userSchema);
 
-module.exports = USER
+module.exports = USER;
--- routes/signup.js
+++ routes/signup.js
@@ -2,15 +2,13 @@ const mongoose = require("mongoose");
 const User = require("../models/users");
 var db = mongoose.connection;
 
-
 const signup = async (req, res) => {
-
- 
-  console.log(req)
+  console.log(req);
   const data = {
-    Email : req.body.Email,
+    Email: req.body.Email,
     Password: req.body.Password,
-  }
+    Name: req.body.name,
+  };
 
   const hasEmail = await User.findOne({ Email: req.body.Email });
 
@@ -22,10 +20,8 @@ const signup = async (req, res) => {
       if (err) console.log(err);
       console.log("inserted new user to database");
     });
-    res.json({signup :true})
-
+    res.json({ signup: true });
   }
 };
 
 module.exports = signup;
-
